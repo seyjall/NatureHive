3ad300c2ca391a482e0a1ca7dfac7c12e7a39bc2
seyjall <sejalmandwe@gmail.com>
2025-07-07 17:59:38 +0530

    changing

--- backend/app.js
+++ backend/app.js
@@ -8,10 +8,7 @@ app.use(cors({
     origin : process.env.CORS_ORIGIN ,
     credentials :true 
 }))
-app.options('*', cors({
-  origin: process.env.CORS_ORIGIN,
-  credentials: true
-}));
+
 
 app.use(express.json({limit : "16kb"}))
 
