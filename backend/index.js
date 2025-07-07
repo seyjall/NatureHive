105db7c2a8cb26d5868a872703c2d0849ed74d33
seyjall <sejalmandwe@gmail.com>
2025-07-07 10:23:35 +0530

    changes for env file

--- backend/index.js
+++ backend/index.js
@@ -5,9 +5,9 @@ import {app} from './app.js';
 
 
 
-dotenv.config({
-    path : './.env'
-})
+if (process.env.NODE_ENV !== "production") {
+  dotenv.config({ path: "./.env" });
+}
 
 import mongoose from "mongoose" 
 import { DB_NAME } from "./src/constants.js";
